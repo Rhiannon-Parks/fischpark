1d0377af8be35bbe4fc2b2d351cf62695303d950
Rhiannon <Rhiannon.Parks@gmail.com>
2018-03-06 17:11:08 -0600

    Removing Microsoft.AI

---
+++ FischPark/FischPark/Controllers/PkmnBiomesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FischPark.Controllers
+{
+    public class PkmnBiomesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
